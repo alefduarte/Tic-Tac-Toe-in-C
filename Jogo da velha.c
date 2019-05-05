946eb5caa14b5121564ec79bf63421902c101296
Álef Duarte <alef.duarte@outlook.com>
2019-05-05 14:34:05 -0300

    Added project files

---
+++ Jogo da velha.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   Jogo da velha.c
+ * Author: alef
+ *
+ * Created on 1 de Agosto de 2016, 11:21
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "biblioteca.h"
+
+/*
+ * 
+ */
+
+void main() {
+    char m[3][3];
+    zera(m);
+
+    JogoPrincipal(m);
+}
+
